c47db0577f3ad6b1f69dbb963629301a22b5bd70
kelvins-io <mybaishati@gmail.com>
2021-03-27 15:32:00 +0800

    mongo根据配置选择加载

--- startup/vars.go
+++ startup/vars.go
@@ -25,36 +25,39 @@ func SetupVars() error {
 		}
 	}
 
-	// 初始化mongodb
-	ctx := context.Background()
-	var maxPoolSize = uint64(vars.MongoDBSetting.MaxPoolSize)
-	var minPoolSize = uint64(vars.MongoDBSetting.MinPoolSize)
+	if vars.MongoDBSetting != nil {
+		// 初始化mongodb
+		ctx := context.Background()
+		var maxPoolSize = uint64(vars.MongoDBSetting.MaxPoolSize)
+		var minPoolSize = uint64(vars.MongoDBSetting.MinPoolSize)
 
-	mgoCfg := &qmgo.Config{
-		Uri:         vars.MongoDBSetting.Uri,
-		Database:    vars.MongoDBSetting.Database,
-		MaxPoolSize: &maxPoolSize,
-		MinPoolSize: &minPoolSize,
-		Auth: &qmgo.Credential{
-			AuthMechanism: "",
-			AuthSource:    vars.MongoDBSetting.AuthSource,
-			Username:      vars.MongoDBSetting.Username,
-			Password:      vars.MongoDBSetting.Password,
-			PasswordSet:   false,
-		},
-	}
-	client, err := qmgo.NewClient(ctx, mgoCfg)
-	if err != nil {
-		log.Printf("mongodb connection err: %v", err)
-		return err
-	}
-	err = client.Ping(30) // 30s
-	if err != nil {
-		log.Printf("mongodb ping timeout err: %v", err)
-		return err
-	}
-	db := client.Database(vars.MongoDBSetting.Database)
+		mgoCfg := &qmgo.Config{
+			Uri:         vars.MongoDBSetting.Uri,
+			Database:    vars.MongoDBSetting.Database,
+			MaxPoolSize: &maxPoolSize,
+			MinPoolSize: &minPoolSize,
+			Auth: &qmgo.Credential{
+				AuthMechanism: "",
+				AuthSource:    vars.MongoDBSetting.AuthSource,
+				Username:      vars.MongoDBSetting.Username,
+				Password:      vars.MongoDBSetting.Password,
+				PasswordSet:   false,
+			},
+		}
+		client, err := qmgo.NewClient(ctx, mgoCfg)
+		if err != nil {
+			log.Printf("mongodb connection err: %v", err)
+			return err
+		}
+		err = client.Ping(30) // 30s
+		if err != nil {
+			log.Printf("mongodb ping timeout err: %v", err)
+			return err
+		}
+		db := client.Database(vars.MongoDBSetting.Database)
 
-	vars.MongoDBDatabase = db
+		vars.MongoDBDatabase = db
+	}
+
 	return nil
 }
